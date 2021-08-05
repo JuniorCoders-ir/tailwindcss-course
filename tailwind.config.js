cffbcd87081b35dd94663dec073082c4e0431d87
Mehdi Neysi <dev.mehdineysi@gmail.com>
2021-08-05 21:18:58 +0430

    feat: add session 5 file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: [],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: () => {
+        return {
+          'main-image': 'url(https://images.unsplash.com/photo-1595683363301-1e94594a550d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=334&q=80)'
+        }
+      }
+    },
   },
   variants: {
     extend: {},
